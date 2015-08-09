535e2867198c9f7d75825267847351fd19d42bb0
Dorival Pedroso <pedroso@cpmech.com>
2015-08-09 16:27:24 +1000

    truss test using elastic rod added

--- fem/t_trusses_test.go
+++ fem/t_trusses_test.go
@@ -10,10 +10,10 @@ import (
 	"github.com/cpmech/gosl/chk"
 )
 
-func Test_bridge01(tst *testing.T) {
+func Test_bridge01a(tst *testing.T) {
 
 	//verbose()
-	chk.PrintTitle("bridge01. simple bridge section")
+	chk.PrintTitle("bridge01a. simple bridge section")
 
 	// fem
 	analysis := NewFEM("data/bridge01.sim", "", true, true, false, false, chk.Verbose, 0)
@@ -31,4 +31,27 @@ func Test_bridge01(tst *testing.T) {
 	tolu := 1e-15
 	tols := 1e-9
 	TestingCompareResultsU(tst, "data/bridge01.sim", "cmp/bridge01.cmp", "", tolK, tolu, tols, skipK, chk.Verbose)
+}
+
+func Test_bridge01b(tst *testing.T) {
+
+	//verbose()
+	chk.PrintTitle("bridge01. simple bridge section. ElastRod")
+
+	// fem
+	analysis := NewFEM("data/bridge01erod.sim", "", true, true, false, false, chk.Verbose, 0)
+
+	// run simulation
+	err := analysis.Run()
+	if err != nil {
+		tst.Errorf("Run failed:\n%v", err)
+		return
+	}
+
+	// check
+	skipK := false
+	tolK := 1e-11
+	tolu := 1e-15
+	tols := 1e-9
+	TestingCompareResultsU(tst, "data/bridge01erod.sim", "cmp/bridge01.cmp", "", tolK, tolu, tols, skipK, chk.Verbose)
 }
