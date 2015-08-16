3a195f7bb075250d780b49f8c0e334ac018fc674
Dorival Pedroso <pedroso@cpmech.com>
2015-08-17 09:55:32 +1000

    examples webpage updated: headers fontsize decreased

--- examples/README-to-html.go
+++ examples/README-to-html.go
@@ -61,7 +61,7 @@ func main() {
 <meta charset="utf-8" />
 
 <style>
-h1 {color:#0064cb; font-family:verdana; font-size:300%;}
+h1 {color:#0064cb; font-family:verdana; font-size:200%;}
 h2 {color:#0064cb}
 h3 {color:#0064cb}
 a:hover {background-color:#5397dc;}
